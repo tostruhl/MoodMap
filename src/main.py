448e786caa1389264b7e6c8443a345abe0fe4800
Tessa Struhl <tostruhl@bu.edu>
2023-10-16 16:24:36 -0400

    added database that takes user input, created gitignore file

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 import moodGuesser
 
 def helloWorld():
-    moodGuesser.basicGuesser(5)
+    mood = int(input("Enter mood from 1-5:"))
+    moodGuesser.basicGuesser(mood)
+    moodGuesser.storeData(mood)
 
 if __name__ == "__main__":
     helloWorld()
--- src/moodGuesser.py
+++ src/moodGuesser.py
@@ -1,3 +1,5 @@
+import csv
+from datetime import datetime
 def basicGuesser(currentMood: int):
     futureMood: int = 0
     if currentMood == 5:
@@ -5,3 +7,12 @@ def basicGuesser(currentMood: int):
     else:
         futureMood = currentMood +1
     print(futureMood)
+def storeData(currentMood: int):
+    now = datetime.now()
+    entry = [now, currentMood]
+    with open('../moodData.csv', 'a') as file_obj:
+        writer_obj = csv.writer(file_obj)
+        writer_obj.writerow(entry)
+        file_obj.close()
+
+    
